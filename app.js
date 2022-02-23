a8c2b6bdcef658b6adc18a8cb20a9f33dac7477a
joshpainter <josh@joshpainter.com>
2022-02-23 04:20:34 -0600

    replaced magic numbers with vars

--- app.js
+++ app.js
@@ -8,26 +8,27 @@ const processOrders = async () => {
 
     const discordHook = new Webhook(process.env.DISCORD_WEBHOOK_URL);
 
+    const xchMultiplier = 1000000000000;
+    const catMultiplier = 1000;
     const hg_market_id = process.env.HASHGREEN_MARKET_ID;
-
-    // These are the basic prices that you want to set for your trade. WARNING: As is, this is dumb and will
-    // lose you money!! (Trust me)
-    var askPriceCAT = 500; // You will offer 500 of your CAT in the ask order
-    var askPriceChia = 0.001; // You want 0.001 XCH back in the ask order
-    var bidPriceCAT = 450; // You want 450 of your CAT back in the bid order
-    var bidPriceChia = 0.001; // You will offer 0.001 XCH in the bid order
-    //End basic prices code
-
+    const askPriceCAT = parseFloat(process.env.ASK_PRICE_CAT);
+    const askPriceXCH = parseFloat(process.env.ASK_PRICE_XCH);
+    const askMinOrders = parseFloat(process.env.ASK_MIN_ORDERS);
+    const bidPriceCAT = parseFloat(process.env.BID_PRICE_CAT);
+    const bidPriceXCH = parseFloat(process.env.BID_PRICE_XCH);
+    const bidMinOrders = parseFloat(process.env.BID_MIN_ORDERS);
+    const orderFeeAmount = parseFloat(process.env.ORDER_FEE_AMOUNT) * xchMultiplier;
+    
 
     // This little bit was used as a countdown timer. The bid/ask price drops every minute until it hits a floor.
     // If you don't want this "feature" then comment out these next few lines.
-    var endTime = new Date(Date.parse("2022-02-16T00:00:00"));
-    const total = Date.parse(endTime) - Date.parse(new Date());
-    const minutes = Math.floor(total / 1000 / 60);
-    askPrice = (minutes + 30);
-    if (askPrice <= 350) askPrice = 350;
-    bidPrice = (minutes + 10);
-    if (bidPrice <= 400) bidPrice = 400;
+    // var endTime = new Date(Date.parse("2022-02-16T00:00:00"));
+    // const total = Date.parse(endTime) - Date.parse(new Date());
+    // const minutes = Math.floor(total / 1000 / 60);
+    // askPrice = (minutes + 30);
+    // if (askPrice <= 350) askPrice = 350;
+    // bidPrice = (minutes + 10);
+    // if (bidPrice <= 400) bidPrice = 400;
     // End of countdown timer code
 
 
@@ -35,31 +36,31 @@ const processOrders = async () => {
     // into bid/ask arrays so we can work with them. The filter I use here assumes that all under 550 are my
     // existing offers, but even if they aren't that's ok cause the order book is full.
     const openOrders = await getOrderBook(hg_market_id);
-    const myAsks = openOrders.data.asks.filter(ask => ask[3]<=550 && ask[4]==0.001);
-    const myBids = openOrders.data.bids.filter(bid => bid[3]<=550 && bid[4]==0.001);
+    const myAsks = openOrders.data.asks.filter(ask => ask[3]==askPriceCAT && ask[4]==askPriceXCH);
+    const myBids = openOrders.data.bids.filter(bid => bid[3]==bidPriceCAT && bid[4]==bidPriceXCH);
     console.log(`Woke up and found ${myAsks.length} asks and ${myBids.length} bids...`)
     // End of get order book
-
+    
     // Now let's put together the offer for either the bid or the ask, depending on which one we need to create.
     // We always want to keep at least 10 orders on both sides of the order book but we only want to create one
     // at a time per ORDER_CHECK_INTERVAL.
     // But first we need to go get a dadjoke of course and we should also send it over to Discord.
     var offer = null;
-    if (myAsks.length < 10) {
+    if (myAsks.length < askMinOrders) {
         console.log("Creating ask...");
         var dadJokeResults = await getDadJoke();
         discordHook.send(`Somebody matched an ask! WOOHOO I'M RICH!! ${dadJokeResults.joke}`);
         offer = {
-            1:(askPriceChia * 1000000000000), // 1 always means XCH
-            2:-(askPrice * 1000) // 2 is the first CAT in your wallet. You can get the index of your CAT
+            1:(askPriceXCH * xchMultiplier), // 1 always means XCH
+            2:-(askPriceCAT * catMultiplier) // 2 is the first CAT in your wallet. You can get the index of your CAT
         };        
     }
-    else if (myBids.length < 10) {
+    else if (myBids.length < bidMinOrders) {
         console.log("Creating bid...");
         discordHook.send("Somebody matched a bid!? OH GREAT NOW I'M POOR how can you joke at a time like this");
         offer = {
-            1:-(bidPriceChia * 1000000000000),
-            2:(bidPrice * 1000)
+            1:-(bidPriceXCH * xchMultiplier),
+            2:(bidPriceCAT * catMultiplier)
         };
     }
     else {
@@ -70,7 +71,7 @@ const processOrders = async () => {
     // complexity of Chia is hidden behind that single beautiful createOffer method. It gives us back an
     // Offer File string and we just send that straight up to Hashgreen's uploadOrder API.
     if (offer) {
-        const newOfferResult = await createOffer(offer);
+        const newOfferResult = await createOffer(offer, orderFeeAmount);
         const uploadResult = await uploadMyOrder(newOfferResult.offer);
         console.log(`Created order with id ${uploadResult.data.id}, sleeping...`);
     }
