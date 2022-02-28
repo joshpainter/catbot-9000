333da9a2c93baabfd8230683b61195570ee91566
joshpainter <josh@joshpainter.com>
2022-02-28 11:31:29 -0600

    remove try/catch

--- api_clients/spacescanClient.js
+++ api_clients/spacescanClient.js
@@ -1,34 +1,28 @@
 const fetch = import('node-fetch');
 const _ = require('lodash');
 const spacescanGetCatsApi = async () => {
-	try {
-		console.info('spacescanGetCatsApi: Fetching results...');
-		const f = await fetch;
-		let cats = new Array();
-		let page = 0;
-		let more = true;
-		do {
-			page++;
-			console.info(`spacescanGetCatsApi: Fetching page ${page}...`);
-			const results = await f.default(
-				`${process.env.SPACESCAN_API_URL}/cats?page=${page}`,
-				{
-					method: 'get',
-					headers: {
-						'Accept': 'application/json',
-					},
+	console.info('spacescanGetCatsApi: Fetching results...');
+	const f = await fetch;
+	let cats = new Array();
+	let page = 0;
+	let more = true;
+	do {
+		page++;
+		console.info(`spacescanGetCatsApi: Fetching page ${page}...`);
+		const results = await f.default(
+			`${process.env.SPACESCAN_API_URL}/cats?page=${page}`,
+			{
+				method: 'get',
+				headers: {
+					'Accept': 'application/json',
 				},
-			);
-			const resultsJson = await results.json();
-			cats = _.union(cats, resultsJson.cats);
-			more = resultsJson.summary.page_count == 100;
-		} while (more);
-		console.info(`spacescanGetCatsApi: Fetched ${cats.length} cats.`);
-		return cats;
-	}
-	catch (error) {
-		console.error(`spacescanGetCatsApi: ${error}`);
-		throw error;
-	}
+			},
+		);
+		const resultsJson = await results.json();
+		cats = _.union(cats, resultsJson.cats);
+		more = resultsJson.summary.page_count == 100;
+	} while (more);
+	console.info(`spacescanGetCatsApi: Fetched ${cats.length} cats.`);
+	return cats;
 };
 module.exports.spacescanGetCatsApi = spacescanGetCatsApi;
--- api_clients/xchTokenClient.js
+++ api_clients/xchTokenClient.js
@@ -1,24 +1,18 @@
 const fetch = import('node-fetch');
 const xchtokenGetTokenApi = async () => {
-	try {
-		console.info('xchtokenGetTokenApi: Fetching results...');
-		const f = await fetch;
-		const results = await f.default(
-			`${process.env.XCHTOKEN_API_URL}`,
-			{
-				method: 'get',
-				headers: {
-					'Accept': 'application/json',
-				},
+	console.info('xchtokenGetTokenApi: Fetching results...');
+	const f = await fetch;
+	const results = await f.default(
+		`${process.env.XCHTOKEN_API_URL}`,
+		{
+			method: 'get',
+			headers: {
+				'Accept': 'application/json',
 			},
-		);
-		const resultsJson = await results.json();
-		console.info(`xchtokenGetTokenApi: Fetched ${resultsJson.data.length} tokens.`);
-		return resultsJson.data;
-	}
-	catch (error) {
-		console.error(`xchtokenGetTokenApi: ${error}`);
-		throw error;
-	}
+		},
+	);
+	const resultsJson = await results.json();
+	console.info(`xchtokenGetTokenApi: Fetched ${resultsJson.data.length} tokens.`);
+	return resultsJson.data;
 };
 module.exports.xchtokenGetTokenApi = xchtokenGetTokenApi;
