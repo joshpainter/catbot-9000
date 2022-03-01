70f426fef6df620ef0839976422ae41db8eee9ec
joshpainter <josh@joshpainter.com>
2022-03-01 00:09:56 -0600

    better error handling/logging

--- workers/discord/events/CatAutocompleteInteraction.js
+++ workers/discord/events/CatAutocompleteInteraction.js
@@ -1,28 +1,34 @@
 const { TokenController } = require('../../../controllers/TokenController');
 module.exports = {
 	name: 'interactionCreate',
+	customName: 'CatAutocompleteInteraction',
 	async execute(interaction) {
-		if (!interaction.isAutocomplete()) return;
-		const query = interaction.options.getString('search');
-		const responses = [];
-		if (query?.length >= 1) {
-			responses.push({
-				name: query,
-				value: query,
-			});
-		}
-		if (query?.length >= 3) {
-			console.info(`autocompleteInteraction: ${query}`);
-			const tc = new TokenController();
-			const tokens = await tc.search(query);
-			for (const token of tokens) {
+		try {
+			if (!interaction.isAutocomplete()) return;
+			const query = interaction.options.getString('search') || '';
+			const responses = [];
+			if (query) {
 				responses.push({
-					name: `${token.name} (${token.symbol})`,
-					value: token.symbol,
+					name: query,
+					value: query,
 				});
-				if (responses.length >= 25) break;
 			}
+			if (query.length >= 3) {
+				const tc = new TokenController();
+				const tokens = await tc.search(query);
+				for (const token of tokens) {
+					responses.push({
+						name: `${token.name} (${token.symbol})`,
+						value: token.symbol,
+					});
+					if (responses.length >= 25) break;
+				}
+				console.info(`${interaction.guild.name}:${this.customName}:INFO:search=${query} returning ${responses.length} matches`);
+			}
+			await interaction.respond(responses);
 		}
-		await interaction.respond(responses);
+		catch (error) {
+			console.error(`${interaction.guild.name}:${this.customName}:ERROR:${error}`);
+		}
 	},
 };
