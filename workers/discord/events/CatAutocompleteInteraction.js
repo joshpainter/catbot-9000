5b5733755ad48b8dafd1b928fcbe9c0a6f5122e6
joshpainter <josh@joshpainter.com>
2022-03-01 01:51:18 -0600

    better logging/error handling

--- workers/discord/events/CatAutocompleteInteraction.js
+++ workers/discord/events/CatAutocompleteInteraction.js
@@ -6,6 +6,7 @@ module.exports = {
 		try {
 			if (!interaction.isAutocomplete()) return;
 			const query = interaction.options.getString('search') || '';
+			// this.logInfo(interaction, `searching for '${query}'...`);
 			const responses = [];
 			if (query) {
 				responses.push({
@@ -18,17 +19,19 @@ module.exports = {
 				const tokens = await tc.search(query);
 				for (const token of tokens) {
 					responses.push({
-						name: `${token.name} (${token.symbol})`,
-						value: token.symbol,
+						name: token.name,
+						value: token.name,
 					});
 					if (responses.length >= 25) break;
 				}
-				console.info(`${interaction.guild.name}:${this.customName}:INFO:search=${query} returning ${responses.length} matches`);
+				this.logInfo(interaction, `search for '${query}' returned ${tokens.length} matches`);
 			}
 			await interaction.respond(responses);
 		}
 		catch (error) {
-			console.error(`${interaction.guild.name}:${this.customName}:ERROR:${error}`);
+			this.logError(interaction, error);
 		}
 	},
+	logInfo: (interaction, logText) => console.info(`${interaction.guild.name}:${module.exports.customName}:INFO:> ${logText}`),
+	logError: (interaction, error) => console.error(`${interaction.guild?.name}:${module.exports.customName}:ERROR:> ${error}`),
 };
