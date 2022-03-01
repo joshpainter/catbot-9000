294e54d669e211e1bdd22bcb0cdc9345670acb93
joshpainter <josh@joshpainter.com>
2022-03-01 03:42:59 -0600

    add logging/error handling

--- workers/discord/commands/cat.js
+++ workers/discord/commands/cat.js
@@ -13,31 +13,29 @@ module.exports = {
 			.setDescription('Enter the TAIL, symbol or name of the CAT (e.g. CATMOS)')
 			.setAutocomplete(true)
 			.setRequired(true)),
+	customName: 'CatCommand',
 	async execute(interaction) {
 		const query = interaction.options.getString('search');
-		console.info(`CatCommand:INFO:${interaction.guild.name}> /cat search ${query} `);
+		this.logInfo(interaction, `search=${query}`);
 		const tc = new TokenController();
 		const allTokens = await tc.fetch();
 		const tokens = await tc.search(query);
-		console.info(`CatCommand:INFO:${interaction.guild.name}> found ${tokens.length} matches for ${query} `);
+		this.logInfo(interaction, `found ${tokens.length} matches for ${query}`);
 		if (!tokens.length) {
 			const dadJoke = await getDadJoke();
 			await interaction.reply(`Couldn't find \`\`${query}\`\`, sorry! Would a joke cheer you up? Yeah? Ok! ${dadJoke.joke}`);
+			this.logInfo(interaction, 'told a (probably terrible) dad joke');
 		}
 		else {
-			const selectedToken = tokens[0];
+			const selectedToken = tokens.shift();
 			const catEmbeds = await CatConfigureEmbeds(interaction, selectedToken);
 			const catComponents = await CatConfigureComponents(interaction, selectedToken);
 			if (tokens.length > 1) {
-				const row = new MessageActionRow()
-					.addComponents(
-						new MessageSelectMenu()
-							.setCustomId('select-cat')
-							.setPlaceholder(`${tokens.length} more results${tokens.length >= 27 ? ', only first 25 shown' : ''}...`),
-					);
-				let counter = 0;
+				const selectMenu = new MessageSelectMenu()
+					.setCustomId('select-cat')
+					.setPlaceholder(`${tokens.length} more results${tokens.length > 25 ? ', only first 25 shown' : ''}...`);
 				for (const token of tokens) {
-					row.components[0].addOptions([
+					selectMenu.addOptions([
 						{
 							label: `${token.name} (${token.symbol})`,
 							// description: token.tail,
@@ -47,10 +45,9 @@ module.exports = {
 							}),
 						},
 					]);
-					counter++;
-					if (counter >= 25) break;
+					if (selectMenu.options.length >= 25) break;
 				}
-				catComponents.push(row);
+				catComponents.push(new MessageActionRow().addComponents(selectMenu));
 			}
 			await interaction.reply({
 				content:`Sifting through ${allTokens.length} CATS to find \`\`${query}\`\`, one sec...`,
@@ -59,4 +56,5 @@ module.exports = {
 			});
 		}
 	},
+	logInfo: (interaction, logText) => console.info(`${interaction?.guild?.name}:${module.exports.customName}:INFO:> ${logText}`),
 };
