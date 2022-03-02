a8c073ae8fa6034711dbaa78924e7cb65e42dc78
joshpainter <josh@joshpainter.com>
2022-03-02 05:05:50 -0600

    additional error handling

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const refreshTokenCache = async () => {
 const start = async () => {
 	await startMarketMaker();
 	await refreshTokenCache();
-
 	setInterval(startMarketMaker, process.env.ORDER_CHECK_INTERVAL * 1000);
 	setInterval(refreshTokenCache, process.env.TOKEN_CACHE_LIFETIME * 1000);
-
 	await startDiscordBot();
 };
 
 start();
+
+process.on('uncaughtException', error => console.error(error));
